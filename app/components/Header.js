d21d52e60aba6e4344e85bfec64decbd80abbbc9
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2023-01-06 19:35:03 +0000

    fix

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,9 +7,6 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 export default function Header() {
 
     const { darkTheme, toggleThemeHandler } = useContext(ThemeContext);
-    console.log(darkTheme);
-
-
     return (
         <header>
             <nav className='dark:bg-gray-900 bg-[#F2DEBA] font-semibold px-10 flex flex-wrap item-center justify-between mx-auto py-3 w-100'>
